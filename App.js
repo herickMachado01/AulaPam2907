77c0c8a48134858b29898dd43b290f37451f272d
Herick <herickmartins86@gmail.com>
2024-07-29 11:28:08 -0300

    Fim do Projeto

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack"; // Fixed import statement
+
+import TelaJavaScript from "./componentes/TelaJavaScript";
+import TelaReact from "./componentes/TelaReact";
+import TelaNode from "./componentes/TelaNode";
+
+const Stack = createStackNavigator(); // Use `Stack` instead of `MenuNav` for clarity
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="JavaScript" component={TelaJavaScript} />
+        <Stack.Screen name="Node" component={TelaNode} />
+        <Stack.Screen name="React" component={TelaReact} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
